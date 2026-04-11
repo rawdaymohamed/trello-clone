314b5fac030c623f925ec77dfbae4b42af0b00cf
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-11 02:23:23 +0200

    Fix: remove vercel.json on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,6 @@ app.use(
     credentials: true, // Essential for HttpOnly Cookies!
   }),
 );
-app.options("*", cors());
 const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 
