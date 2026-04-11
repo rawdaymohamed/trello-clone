a3aa484087500e1b0c5e95ab8a7f0a37dfae3e34
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-11 02:09:15 +0200

    Fix: fix baseUrl in redux auth

--- client/src/store/api/authApi.ts
+++ client/src/store/api/authApi.ts
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+const baseUrl = import.meta.env.VITE_API_URL;
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+    baseUrl: baseUrl,
+    credentials: "include", // Important for sending cookies
   }),
   endpoints: (builder) => ({
     register: builder.mutation({
